ff97a72a2d7f9096a31aaea10e24bc4bb9100ed8
Kevin-LeBlanc <keleblanc24@gmail.com>
2017-02-08 16:30:08 -0500

    Few changes
    End of day publish

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 (function(){
-  var app = angular.module('site', [ ]);
+  var app = angular.module('site', ['ngRoute']);
+
+  app.config(function($routeProvider){
+    $routeProvider
+
+    .when('/', {
+      templateUrl: 'pages/home.html',
+      controller:  'SiteController'
+    })
+    .when('/creator', {
+      templateUrl: 'pages/creator.html',
+      controller:  'CharCreatorController'
+    });
+  });
 
   app.controller('SiteController', function($scope){
     $scope.message = 'Hello!';
