f8c1e5c779236cdaa0c850b7beeee359bc387d8c
Kevin-LeBlanc <keleblanc24@gmail.com>
2017-03-02 16:26:54 -0500

    Revamping the clan/bloodline lists
    Redid the clan list in controller, made the filter & clan list
    relationship better.

--- clanController.js
+++ clanController.js
@@ -1,84 +1,69 @@
 var app = angular.module("site");
 
-app.controller("ClanController", function($scope, $window, $http){
+app.controller("ClanController", function($window, $http){
 
   var vm = this;
   vm.clanDescriptions = [];
+  vm.clanPage = "clans/clanpage.html";
+  vm.showClanInfo = false;
+  vm.showBloodlineInfo = false;
+  vm.selectedBloodline = null;
+
   $http.get('clans/clanDescriptions.txt').then(function(response){
     vm.clanDescriptions = response.data;
   })
 
-  this.clanPage = "clans/clanpage.html";
-  this.showClanInfo = false;
-  this.showBloodlineInfo = false;
+  vm.filterClans = filterClans;
 
-  this.hasBloodlines = function(clan){
-    if((clan != 'Cappadocian') && (clan != 'Gangrel') && (clan != 'Lasombra') && (clan != 'N/A') )
-    return false;
-    else
-      return true;
-  }
+  vm.clanFilters = ["All", "Thirteen", "Camarilla", "Sabbat", "Independent", "All Clans",
+                      "All Bloodlines", "Camarilla (clans only)", "Sabbat (clans only)", "Dark Ages", "High clans", "Low clans"];
+  vm.clanList = [{id:0,  name:"Ahrimanes", filters:["Sabbat", "All Bloodlines", "Dark Ages"]},
+                  {id:1,  name:"Anda", filters:["Independent", "All Bloodlines", "Dark Ages"]},
+                  {id:2,  name:"Assamite", filters:["Thirteen", "Independent", "All Clans", "Low Clans", "Dark Ages"]},
+                  {id:3,  name:"Baali", filters:["Independent", "All Bloodlines", "Dark Ages"]},
+                  {id:4,  name:"Blood Brothers", filters:["Sabbat", "All Bloodlines"]},
+                  {id:5,  name:"Brujah", filters:["Thirteen", "Camarilla", "All Clans", "Camarilla (clans only)", "High Clans", "Dark Ages"]},
+                  {id:6,  name:"Caitiff", filters:["All Clans", "Dark Ages"]},
+                  {id:7,  name:"Cappadocian", filters:["All Clans", "Dark Ages", "High Clans"]},
+                  {id:8,  name:"Children of Osiris", filters:["All Bloodlines", "Dark Ages"]},
+                  {id:9,  name:"Daughters of Cacophony", filters:["All Bloodlines"]},
+                  {id:10, name:"Followers of Set", filters:["Thirteen", "Independent", "All Clans", "Dark Ages", "Low Clans"]},
+                  {id:11, name:"Gargoyles", filters:["All Bloodlines"]},
+                  {id:12, name:"Gangrel", filters:["Thirteen", "Independent", "All Clans", "Low Clans", "Dark Ages"]},
+                  {id:13, name:"Giovanni", filters:["Thirteen", "Independent", "All Clans", "Dark Ages"]},
+                  {id:14, name:"Harbingers of Skulls", filters:["Sabbat", "All Bloodlines"]},
+                  {id:15, name:"Kiasyd", filters:["All Bloodlines", "Dark Ages"]},
+                  {id:16, name:"Lamia", filters:["All Bloodlines", "Dark Ages"]},
+                  {id:17, name:"Lasombra", filters:["Thirteen", "Sabbat", "All Clans", "Sabbat (clans only)", "Dark Ages", "High Clans"]},
+                  {id:18, name:"Lhiannan", filters:["Independent", "All Bloodlines", "Dark Ages"]},
+                  {id:19, name:"Malkavian", filters:["Thirteen", "Camarilla", "All Clans", "Camarilla (clans only)", "Dark Ages", "Low Clans"]},
+                  {id:20, name:"Nagaraja", filters:["All Bloodlines", "Dark Ages"]},
+                  {id:21, name:"Noiad", filters:["All Bloodlines", "Dark Ages"]},
+                  {id:22, name:"Nosferatu", filters:["Thirteen", "Camarilla", "All Clans", "Camarilla (clans only)", "Dark Ages", "Low Clans"]},
+                  {id:23, name:"Ravnos", filters:["Thirteen", "Independent", "All Clans", "Dark Ages", "Low Clans"]},
+                  {id:24, name:"Salubri", filters:["Independent", "All Clans", "Dark Ages"]},
+                  {id:25, name:"Samedi", filters:["All Bloodlines"]},
+                  {id:26, name:"Toreador", filters:["Thirteen", "Camarilla", "All Clans", "Camarilla (clans only)", "Dark Ages", "High Clans"]},
+                  {id:27, name:"Tremere", filters:["Thirteen", "Camarilla", "All Clans", "Camarilla (clans only)", "Dark Ages", "Low Clans"]},
+                  {id:28, name:"Tzimisce", filters:["Thirteen", "Sabbat", "All Clans", "Sabbat (clans only)", "Dark Ages", "High Clans"]},
+                  {id:29, name:"Ventrue", filters:["Thirteen", "Camarilla", "All Clans", "Camarilla (clans only)", "Dark Ages", "High Clans"]}
+                ];
 
-  this.clanList = [
-    {id: 0,  name: "N/A", filters:["Bloodlines"], bloodlines: [
-      {id: 0,  name: "Baali", clan: null},
-      {id: 1,  name: "Blood Brothers", clan: null},
-      {id: 2,  name: "Children of Osiris", clan: null},
-      {id: 3,  name: "Daughters of Cacophony", clan: null},
-      {id: 4,  name: "Gargoyles", clan: null},
-      {id: 5, name: "Nagaraja", clan: null},
-      {id: 6, name: "True Brujah", clan: null}
-    ]},
-    {id: 1,  name: "Assamite", filters:["Independent", "Thirteen"], bloodlines: false},
-    {id: 2,  name: "Brujah", filters:["Thirteen", "Camarilla"], bloodlines: false},
-    {id: 3,  name: "Caitiff", filters:["Independent"], bloodlines: false},
-    {id: 4,  name: "Cappadocian", filters:["Independent", "Dark Ages"], bloodlines: [
-      {id: 0, name: "None"},
-      {id: 1, name: "Harbingers of Skulls"},
-      {id: 2, name: "Lamia"},
-      {id: 3, name: "Samedi"}
-    ]},
-    {id: 5,  name: "Followers of Set", filters:["Independent", "Thirteen"], bloodlines: false},
-    {id: 6,  name: "Gangrel", filters:["Independent", "Thirteen"], bloodlines: [
-      {id: 0, name: "None"},
-      {id: 1, name: "Ahrimanes", clan: "Gangrel"},
-      {id: 2, name: "Anda"},
-      {id: 3, name: "Lhiannan"},
-      {id: 4, name: "Noiad"},
-    ]},
-    {id: 7,  name: "Giovanni", filters:["Independent", "Thirteen"], bloodlines: false},
-    {id: 8,  name: "Lasombra", filters:["Sabbat", "Thirteen"], bloodlines: [
-      {id: 0, name: "None"},
-      {id: 1, name: "Kiasyd"}
-    ]},
-    {id: 9, name: "Malkavian", filters:["Camarilla", "Thirteen"], bloodlines: false},
-    {id: 10, name: "Nosferatu", filters:["Camarilla", "Thirteen"], bloodlines: false},
-    {id: 11, name: "Ravnos", filters:["Independent", "Thirteen"], bloodlines: false},
-    {id: 12, name: "Salubri", filters:["Independent"], bloodlines: false},
-    {id: 13, name: "Toreador", filters:["Camarilla", "Thirteen"], bloodlines: false},
-    {id: 14, name: "Tremere", filters:["Camarilla", "Thirteen"], bloodlines: false},
-    {id: 15, name: "Tzimisce", filters:["Sabbat", "Thirteen"], bloodlines: false},
-    {id: 16, name: "Ventrue", filters:["Camarilla", "Thirteen"], bloodlines: false}
-  ];
-  vm.filteredClanList = [];
-
-  this.filterClans = function(filter){
+  function filterClans(filter){
     vm.filteredClanList = [];
     if(filter==='All'){
       vm.filteredClanList = vm.clanList;
       return;
     }
-    this.clanList.forEach(function(clan){
+    vm.clanList.forEach(function(clan){
       if(clan.filters.includes(filter)){
         vm.filteredClanList.push(clan);
       }
     })
+    vm.selectedClan = vm.filteredClanList[0];
   };
 
-  this.clanFilters = ["All", "Thirteen", "Camarilla", "Sabbat","Independent",
-                      "Bloodlines", "Camarilla (clans only)", "Sabbat (clans only)"];
-                      // "Dark Ages", "High clans", "Low clans"];
-
-  this.selectedClan = this.filteredClanList[0];
-  this.selectedBloodline = null;
+  vm.filteredClanList = vm.clanList;
+  vm.selectedClan = vm.filteredClanList[0];
+  vm.selectedClanFilter = vm.clanFilters[0];
 });
