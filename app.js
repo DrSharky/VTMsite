c83ef56deebd83dd553389235477298458c98baf
kleb <keleblanc24@gmail.com>
2017-03-09 11:31:37 -0500

    Revert "Moved stuff to attribute controller, trying to change routing"

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function(){
-  var app = angular.module("site", ["ngRoute", "ui.router"]);
+  var app = angular.module("site", ["ngRoute"]);
 
   app.controller("NavController", function($location){
     this.isActive = function(viewLocation){
@@ -7,22 +7,17 @@
     }
   });
 
-  app.config(function($stateProvider, $urlRouterProvider, $locationProvider){
-    $stateProvider
-      .state("home",{
-        url: "/home",
+  app.config(function($routeProvider){
+    $routeProvider
+      .when("/home",{
         templateUrl: "home.html",
         controller: "HomeController as HomeCtrl"
       })
-      .state("creator",{
-        url: "/creator",
+      .when("/creator",{
         templateUrl: "creator.html",
         controller: "CharCreatorController as creatorCtrl"
-      });
-      // .otherwise({redirectTo:"/home"});
-      $urlRouterProvider.otherwise("/home");
-      $locationProvider.html5Mode(true);
-
+      })
+      .otherwise({redirectTo:"/home"});
     });
   }()
 );
