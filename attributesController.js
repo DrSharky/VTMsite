6dfa94fea97738cb22aff9a5b18a1bc00a47567e
DrSharky <keleblanc24@gmail.com>
2017-03-10 09:49:50 -0500

    Redid some changes
    I lost these changes somehow? strange.

--- attributesController.js
+++ attributesController.js
@@ -77,49 +77,4 @@ function categoryChange(changedCategory, id){
   })
 }
 
-class Attributes {
-  constructor(){
-    this.points = [{id:0, img:"./full.png"},
-                   {id:1, img:"./empty.png"},
-                   {id:2, img:"./empty.png"},
-                   {id:3, img:"./empty.png"},
-                   {id:4, img:"./empty.png"}];
-
-    this.select = function(index){
-      if(this.points[index].img=="./full.png")
-      {
-        this.points.forEach(function(point){
-          if(point.id <= index){
-            var test = 1;
-          }
-          else{
-            point.img = "./empty.png";
-          }
-        });
-      }
-      if(this.points[index].img=="./empty.png")
-      {
-        this.points.forEach(function(point){
-          if(point.id > index){
-            var test = 1;
-          }
-          else{
-            point.img = "./full.png";
-          }
-        });
-      }
-    }
-  }
-}
-
-this.strength = new Attributes();
-this.dexterity = new Attributes();
-this.stamina = new Attributes();
-this.charisma = new Attributes();
-this.manipulation = new Attributes();
-this.appearance = new Attributes();
-this.perception = new Attributes();
-this.intelligence = new Attributes();
-this.wits = new Attributes();
-
 });
