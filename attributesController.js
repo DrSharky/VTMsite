42ae1e59e400346ccfa1a1f9b5609ed4a10e36db
DrSharky <keleblanc24@gmail.com>
2017-03-13 16:23:57 -0400

    Attempting to code the point rules
    Need to organize this better, not very good so far.

--- attributesController.js
+++ attributesController.js
@@ -4,6 +4,9 @@ app.controller("AttributesController", function($scope, UglyService){
 
 
   this.categoryChange = categoryChange;
+  this.selectAttribute = selectAttribute;
+  this.getPriority = getPriority;
+  this.getPriorityPts = getPriorityPts;
 
   this.attributePriorities = ["Primary", "Secondary", "Tertiary"];
   this.attributesPage = "./attributes.html";
@@ -95,15 +98,76 @@ this.isUglyClan = function(){
     }
   };
 
-
-//TODO: Get the point limits to work based on the category priorities.
-function selectAttribute(attribute, index){
-  switch(attribute){
-    case "strength":
-      this.strength.select(index);
-
+function getPriority(attribute){
+  if(attribute == "strength" || "dexterity" || "stamina"){
+    return this.attributeCategories[0].priority;
+  }
+  if(attribute == "charisma" || "manipulation" || "appearance"){
+    return this.attributeCategories[1].priority;
+  }
+  if(attribute == "perception" || "intelligence" || "wits"){
+    return this.attributeCategories[2].priority;
   }
 }
+
+function getPriorityPts(priority){
+  switch(priority){
+    case "Primary":
+      return this.primaryPts;
+      break;
+    case "Secondary":
+      return this.secondaryPts;
+      break;
+    case "Tertiary":
+      return this.tertiaryPts;
+      break;
+    default:
+      break;
+  }
+}
+
+
+//TODO: Get the point limits to work based on the category priorities.
+//TODO: Record the current number at which the attribute is assigned (i.e. 2, 3, 4, 5) so that the math here will be easier.
+function selectAttribute(attribute, index){
+  var priority = this.getPriority(attribute);
+  var priorityPts = this.getPriorityPts(priority);
+  switch(attribute){
+    case "strength":
+      if((priorityPts - index > 0)){
+        this.strength.select(index);
+        // if(priority == "Primary") ?? Maybe not...
+        //   this.primaryPts -= 1;
+      }
+      break;
+    case "dexterity":
+      this.strength.select(index);
+      break;
+    case "stamina":
+      this.stamina.select(index);
+      break;
+    case "charisma":
+      this.charisma.select(index);
+      break;
+    case "manipulation":
+      this.manipulation.select(index);
+      break;
+    case "appearance":
+      this.appearance.select(index);
+      break;
+    case "perception":
+      this.perception.select(index);
+      break;
+    case "intelligence":
+      this.intelligence.select(index);
+      break;
+    case "wits":
+      this.wits.select(index);
+      break;
+    default:
+      break;
+  }
+};
 
 function categoryChange(changedCategory, id){
   this.attributeCategories[id].priority = changedCategory;
