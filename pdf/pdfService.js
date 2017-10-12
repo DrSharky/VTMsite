a2a5d6415175a20965062f0e65c97dbbc035749d
DrSharky <keleblanc24@gmail.com>
2017-10-12 00:28:10 -0400

    Adding pdf export fields
    It's actually working...

--- pdf/pdfService.js
+++ pdf/pdfService.js
@@ -1,7 +1,7 @@
 var app = angular.module("site");
 app.service('PdfService',
-['$http', 'CharCreatorService', 'ClanService',
-function($http, CharCreatorService, ClanService){
+['$http', 'CharCreatorService', 'ClanService', 'AttributesService',
+function($http, CharCreatorService, ClanService, AttributesService){
 
   this.imgData = "";
   this.generatePDF = generatePDF;
@@ -19,12 +19,37 @@ function($http, CharCreatorService, ClanService){
     doc.addImage(this.imgData, 'JPEG', 0, 0, width, height);
     doc.setFontSize(11);
 
-    doc.text(33, 41.8, CharCreatorService.charName);
+    doc.text(32.9, 41.8, CharCreatorService.charName);
     doc.text(91.5, 41.8, CharCreatorService.charNature);
     doc.text(144.7, 41.8, ClanService.selectedClan.name);
-    doc.text(34.1, 47.6, CharCreatorService.charPlayer);
-    doc.text(97.2, 47.6, CharCreatorService.charDemeanor);
-    doc.text(155.8, 47.6, CharCreatorService.charGeneration);
+    doc.text(34.1, 47.7, CharCreatorService.charPlayer);
+    doc.text(97.2, 47.7, CharCreatorService.charDemeanor);
+    doc.text(155.8, 47.7, CharCreatorService.charGeneration);
+    doc.text(39.4, 53.5, CharCreatorService.charChronicle);
+    doc.text(93.2, 53.5, CharCreatorService.charConcept);
+    doc.text(144.1, 53.5, CharCreatorService.charSire);
+
+    doc.setDrawColor(0);
+    doc.setFillColor(0);
+    var strengthPosition = 59.74;
+    doc.circle(strengthPosition, 71.52, 1.28, 'FD');
+
+    for(var strengthPoint in AttributesService.strength.points){
+      if(AttributesService.strength.points[strengthPoint].id ||
+         AttributesService.strength.points[strengthPoint].type == ""){
+        strengthPosition += 2.87;
+        if(AttributesService.strength.points[strengthPoint].type == "original"){
+          doc.setDrawColor(0);
+          doc.setFillColor(0);
+          doc.circle(strengthPosition, 71.52, 1.28, 'FD');
+        }
+        else if(AttributesService.strength.points[strengthPoint].type == "freebie"){
+          doc.setDrawColor(187, 0, 0);
+          doc.setFillColor(187, 0, 0);
+          doc.circle(strengthPosition, 71.52, 1.28, 'FD');
+        }
+      }
+    }
 
     doc.save("character_sheet.pdf");
   }
